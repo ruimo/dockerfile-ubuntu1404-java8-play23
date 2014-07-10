c3dd170f68259dec831cb60ddbaaed3fdcbef723
Ruimo Uno <ruimo.uno@gmail.com>
2014-07-10 15:04:51 +0900

    Add argument.

--- launch.sh
+++ launch.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -e "HOST_UID=`id -u`" -e "HOST_USER=$USER" -p 9000:9000 -v $HOME:/var/home:rw -i -t ruimo/dockerfile-ubuntu1404-java8-pl
+docker run -e "HOST_UID=`id -u`" -e "HOST_USER=$USER" -p 9001:9000 -v $HOME:/var/home:rw -i -t ruimo/dockerfile-ubuntu1404-java8-pl $*
